8b4518e87e2e9b2a70bade457a5b46a798d5a4c6
Adrian Meister <adrian.zubarev@devandartist.com>
2020-04-23 15:47:08 +0200

    Make static builder methods available

--- Sources/Layout/Core/LayoutBuilder.swift
+++ Sources/Layout/Core/LayoutBuilder.swift
@@ -1,27 +1,27 @@
 // FIXME: Does not support single expression yet.
 @_functionBuilder
 public enum LayoutBuilder {
-  typealias Component<Content> = [AnyLayout<Content>]
+  public typealias Component<Content> = [AnyLayout<Content>]
 
-  static func buildExpression<T>(
+  public static func buildExpression<T>(
     _ expression: T
   ) -> [AnyLayout<T.Content>] where T: Layout {
     [expression.wrapIntoAnyLayout()]
   }
 
-  static func buildIf<Content>(
+  public static func buildIf<Content>(
     _ children: Component<Content>?
   ) -> Component<Content> {
     children ?? []
   }
 
-  static func buildBlock<Content>(
+  public static func buildBlock<Content>(
     _ component: Component<Content>
   ) -> Component<Content> {
     component
   }
 
-  static func buildBlock<Content>(
+  public static func buildBlock<Content>(
     _ children: Component<Content>...
   ) -> Component<Content> {
     // FIXME: Use `.flatMap(\.self)` when compiler bug is resolved.
