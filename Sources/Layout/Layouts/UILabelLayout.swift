588aac8397ecd91af9210ace7f95b79204e959df
Adrian Meister <adrian.zubarev@devandartist.com>
2020-09-17 08:02:59 +0200

    Update UILabelLayout

--- Sources/Layout/Layouts/UILabelLayout.swift
+++ Sources/Layout/Layouts/UILabelLayout.swift
@@ -1,8 +1,53 @@
-//
-//  File.swift
-//  
-//
-//  Created by Adrian Meister on 17.09.20.
-//
+#if canImport(UIKit)
+import UIKit
 
-import Foundation
+public struct UILabelLayout<View>: Layout where View: UILabel {
+  public typealias Content = UIView
+
+  public let view: View
+
+  // FIXME: Move these into its own modifier.
+  let fixedHorizontalSize: Bool
+  let fixedVerticalSize: Bool
+
+  public init(
+    _ view: View,
+    fixedHorizontalSize: Bool = false,
+    fixedVerticalSize: Bool = false
+  ) {
+    self.view = view
+    self.fixedHorizontalSize = fixedHorizontalSize
+    self.fixedVerticalSize = fixedVerticalSize
+  }
+
+  public func build(_ traits: inout LayoutTraits) {
+    sanitizeSize(traits.proposedSize)
+
+    let contentSize = view.intrinsicContentSize
+    let fittingSize = view.sizeThatFits(traits.proposedSize)
+
+    var size = traits.proposedSize
+    if contentSize.width != UIView.noIntrinsicMetric {
+      size.width = fittingSize.width
+    }
+    if contentSize.height != UIView.noIntrinsicMetric {
+      size.height = fittingSize.height
+    }
+
+    if fixedHorizontalSize == false, size.width > traits.proposedSize.width {
+      size.width = traits.proposedSize.width
+    }
+    if fixedVerticalSize == false, size.height > traits.proposedSize.height {
+      size.height = traits.proposedSize.height
+    }
+
+    sanitizeSize(size)
+    traits.dimensions = ViewDimensions(size: size)
+  }
+
+  public func layout(in rect: CGRect, using traits: inout LayoutTraits) {
+    precondition(rect.size == CGSize(traits.dimensions))
+    view.frame = rect
+  }
+}
+#endif
